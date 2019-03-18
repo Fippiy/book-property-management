645ca02c175d8da503900402a67c4e0f5dd4121f
Fippiy <profier4@gmail.com>
2019-03-18 20:46:16 +0900

    setting index sidebar to components

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+  <div class="sidebar__title">カテゴリ一覧</div>
+  <ul class="sidebar__list">
+    <li class="sidebar__list--name">カテゴリ1</li>
+    <li class="sidebar__list--name">カテゴリ2</li>
+    <li class="sidebar__list--name">カテゴリ3</li>
+  </ul>
+</div>
