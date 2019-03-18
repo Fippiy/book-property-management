09c91c501d0e76721f16065fb4dd735c55e9673e
Fippiy <profier4@gmail.com>
2019-03-18 16:19:44 +0900

    setting books table migration

--- database/migrations/2019_03_18_071112_create_books_table.php
+++ database/migrations/2019_03_18_071112_create_books_table.php
@@ -14,7 +14,9 @@ class CreateBooksTable extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('title');
+            $table->string('picture');
             $table->timestamps();
         });
     }
