212a0e24d6e4cef4834356816a6d829b86e5cf2b
Fippiy <profier4@gmail.com>
2019-03-18 12:21:20 +0900

    setting reset stylesheet

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,5 +1,9 @@
 <html>
 <head>
+  <meta charset="utf-8">
+  <title>TopPage</title>
+  <link href="/css/reset.css" rel="stylesheet" type="text/css">
+  <link href="/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
   testpage
