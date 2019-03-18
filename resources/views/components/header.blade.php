a1fbf03ea8a4bc0d04cd3e7455847b83127eed3c
Fippiy <profier4@gmail.com>
2019-03-18 20:44:06 +0900

    create components header view

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<header class="whole__header">
+  <div class="whole__header--headbar">
+  </div>
+  <div class="whole__header--title">
+    <h1 class="title-name">Book-property</h1>
+  </div>
+  <div class="whole__header--breadcrumbs">
+    トップページ > ライブラリー
+  </div>
+  <div class="whole__header--menutab">
+    <div class="tab mypage">
+      マイページ
+    </div>
+    <div class="tab search">
+      検索
+    </div>
+  </div>
+</header>
