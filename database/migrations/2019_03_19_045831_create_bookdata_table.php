09beb008e9387d85c41034d70c6290f0efadca6e
Fippiy <profier4@gmail.com>
2019-03-19 14:07:30 +0900

    edit migration file setting

--- database/migrations/2019_03_19_045831_create_bookdata_table.php
+++ database/migrations/2019_03_19_045831_create_bookdata_table.php
@@ -14,7 +14,9 @@ class CreateBookdataTable extends Migration
     public function up()
     {
         Schema::create('bookdata', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('title');
+            $table->string('picture');
             $table->timestamps();
         });
     }
