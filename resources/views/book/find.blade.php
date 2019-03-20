02fe1f7bdc51f65ca026321e2188602d7cbd12d3
Fippiy <profier4@gmail.com>
2019-03-20 12:12:17 +0900

    create find view page

---
+++ resources/views/book/find.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.layout')
+
+@section('title', 'FindPage')
+
+@section('stylesheet')
+  <link href="/css/sidebar.css" rel="stylesheet" type="text/css">
+  <link href="/css/book-index.css" rel="stylesheet" type="text/css">
+@endsection
+
+@section('content')
+  <div class="index-content">
+    <!-- サイドバー(コンポーネント) -->
+    @component('components.sidebar')
+    @endcomponent
+    <div class="books-list">
+      <div class="books-list__title">
+        検索
+      </div>
+
+    <form action="/book/find" method="post">
+      {{ csrf_field() }}
+      <input type="text" name="input" value="{{$input}}">
+      <input type="submit" value="find">
+    </form>
+
+
+      <div class="book-table">
+        <table class="book-table__list">
+          <tr>
+            <td>id</td>
+            <td>写真</td>
+            <td>タイトル</td>
+            <td>登録日</td>
+          </tr>
+          @if (isset($book))
+            <tr>
+              <td>{{$book->id}}</td>
+              <td>{{$book->picture}}</td>
+              <td><a href="/book/{{$book->id}}">{{$book->title}}</a></td>
+              <td>{{$book->created_at}}</td>
+            </tr>
+          @endif
+        </table>
+      </div>
+    </div>
+  </div>
+
+@endsection
+
+@section('footer')
+copyright 2017 tuyano.
+@endsection
