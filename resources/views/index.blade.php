f51852790f56a2a6eb46817e65ba04439ce33d06
Fippiy <profier4@gmail.com>
2019-03-23 13:10:13 +0900

    create top page view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>book-property-management</title>
+  <link href="/css/top-page.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div class="toppage-contents">
+    <div class="toppage-contents__top">
+      <h1 class="title">book-property-management</h1>
+    </div>
+    <div class="toppage-contents__main">
+
+      <div class="toppage-contents__main--left-content">
+      </div>
+
+      <div class="toppage-contents__main--right-content">
+        <div class="select-menu">
+          <div class="select-menu__sign-up">
+            <a href="">新規登録</a>
+          </div>
+          <div class="select-menu__sign-in">
+            <a href="">ログイン</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
+
