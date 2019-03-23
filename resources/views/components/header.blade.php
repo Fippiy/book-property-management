2b73c283df58158e8457ae60b2835a781af2d903
Fippiy <profier4@gmail.com>
2019-03-23 14:51:14 +0900

    setting layout link to logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,20 @@
 <header class="book-header">
   <div class="book-header__headbar">
   </div>
-  <div class="book-header__title">
-    <a href="/book"><h1 class="title-name">Book-property</h1></a>
+  <div class="book-header__header">
+    <div class="book-header__header--title">
+      <a href="/book"><h1 class="title-name">Book-property</h1></a>
+    </div>
+    <div class="book-header__header--navbar">
+      <form id="logout-form" action="{{ route('logout') }}" method="POST">
+        @csrf
+        <a class="dropdown-item" href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+      </form>
+    </div>
   </div>
   <div class="book-header__breadcrumbs">
     トップページ > ライブラリー
