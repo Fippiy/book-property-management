f455c4c35e76f3cc0d3c228cc4d40a8998d00241
Fippiy <profier4@gmail.com>
2019-03-23 13:51:40 +0900

    setting top page link to auth

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,10 +17,10 @@
       <div class="toppage-contents__main--right-content">
         <div class="select-menu">
           <div class="select-menu__sign-up">
-            <a href="">新規登録</a>
+            <a href="./register">新規登録</a>
           </div>
           <div class="select-menu__sign-in">
-            <a href="">ログイン</a>
+            <a href="./login">ログイン</a>
           </div>
         </div>
       </div>
