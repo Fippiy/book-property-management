07bda13cfe94a588f16ac185d05a73d7593b8480
Fippiy <profier4@gmail.com>
2019-04-01 17:29:40 +0900

    rewrite edit view page

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -14,7 +14,7 @@
     @endcomponent
     <div class="books-list">
       <div class="books-list__title">
-        詳細ページ
+        詳細
         <div class="books-list__title--navigation">
           <a href="/book/{{$book->id}}/edit" class="nav-btn edit">編集</a>
           <form action="/book/{{$book->id}}" method="post">
@@ -29,11 +29,40 @@
           @if (isset($book->picture))
             <img src="{{$book->picture}}">
           @else
-            No Image
+            <img src="../image/no-entry.jpg">
+            <br>写真は登録されていません。
           @endif
         </div>
         <div class="book-detail__document">
-          <h3 class="book-detail__document--title">{{$book->title}}</h3>
+          <h3 class="document-index">書籍情報</h3>
+          <div class="document-content">
+            <div class="document-content__label">タイトル</div>
+            <div class="document-content__column">{{$book->title}}</div>
+          </div>
+          <div class="document-content">
+            <div class="document-content__label">著者名</div>
+            <div class="document-content__column">テスト(仮置)</div>
+          </div>
+          <div class="document-content">
+            <div class="document-content__label">出版社</div>
+            <div class="document-content__column">テスト(仮置)</div>
+          </div>
+          <div class="document-content">
+            <div class="document-content__label">出版日</div>
+            <div class="document-content__column">1900/1/1(仮置)</div>
+          </div>
+          <div class="document-content">
+            <div class="document-content__label">登録日</div>
+            <div class="document-content__column">{{$book->created_at}}</div>
+          </div>
+          <div class="document-content">
+            <div class="document-content__label">編集日</div>
+            <div class="document-content__column">{{$book->updated_at}}</div>
+          </div>
+          <h4 class="document-index">書籍詳細</h4>
+          <div class="document-content">
+            <div class="document-content__column">{{$book->detail}}</div>
+          </div>
         </div>
       </div>
     </div>
