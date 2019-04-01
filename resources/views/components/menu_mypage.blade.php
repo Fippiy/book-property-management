1a5430825638b9a4228a3eb2d5995b8e2dbb8b07
Fippiy <profier4@gmail.com>
2019-04-01 21:15:48 +0900

    setting menu component

---
+++ resources/views/components/menu_mypage.blade.php
@@ -0,0 +1,16 @@
+<div class="book-header__menutab">
+  <a href="/user">
+    <div class="tab mypage">
+      マイページトップ
+    </div>
+  </a>
+  <div class="tab mypage">
+    ユーザ情報編集
+  </div>
+  <div class="tab mypage">
+    所有書籍登録
+  </div>
+  <div class="tab mypage">
+    所有書籍検索
+  </div>
+</div>
