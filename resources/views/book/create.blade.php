363f51e0a381a9256c9d9392c89025c36d51b96a
Fippiy <profier4@gmail.com>
2019-04-01 14:58:33 +0900

    rewrite create view page

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -3,22 +3,48 @@
 @section('title', 'CreateForm')
 
 @section('stylesheet')
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="/js/update_book.js" type="text/javascript" charset="UTF-8"></script>
   <link href="/css/sidebar.css" rel="stylesheet" type="text/css">
-  <link href="/css/book-form.css" rel="stylesheet" type="text/css">
+  <link href="/css/book-index.css" rel="stylesheet" type="text/css">
 @endsection
 
 @section('content')
-  <div class="book-form-area">
+  <div class="index-content">
     <!-- サイドバー(コンポーネント) -->
     @component('components.sidebar')
     @endcomponent
-    <form action="/book" method="post" class="book-form" enctype="multipart/form-data">
-      {{ csrf_field() }}
-      <table>
-      <tr><th>タイトル</th><td><input type="text" name="title" value="{{old('title')}}"></td></tr>
-      <tr><th>写真</th><td><input type="file" name="picture" value="{{old('picture')}}"></td></tr>
-      <tr><th></th><td><input type="submit" value="send"></td></tr>
-      </table>
-    </form>
+    <div class="books-list">
+      <div class="books-list__title">
+        新規登録
+      </div>
+      <div class="book-new">
+        <form action="/book" method="post" enctype="multipart/form-data">
+          {{ csrf_field() }}
+          <div class="form-contents">
+            <div class="form-left">
+              <div>
+                <div class="form-label">写真</div>
+                <div><input type="file" name="picture"></div>
+                <div class="form-input__picture afterimage"><span class="form-input__picture--text">写真が選択されていません</span></div>
+              </div>
+            </div>
+            <div class="form-right">
+              <div class="form-input">
+                <div class="form-label">タイトル名</div>
+                <div><input class="form-input__title" type="text" name="title" value="{{old('title')}}"></div>
+              </div>
+              <div class="form-input">
+                <div class="form-label">詳細</div>
+                <div><textarea class="form-input__detail" type="text" name="title" value="{{old('title')}}"></textarea></div>
+              </div>
+            </div>
+          </div>
+          <div class="form-foot">
+            <input class="send" type="submit" value="登録">
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 @endsection
