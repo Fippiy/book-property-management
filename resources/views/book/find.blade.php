273bcc139f5e684d8a0bfa7a19c9a7582de0e7a1
Fippiy <profier4@gmail.com>
2019-04-01 21:17:29 +0900

    resetting compornent to book views

--- resources/views/book/find.blade.php
+++ resources/views/book/find.blade.php
@@ -8,11 +8,10 @@
 @endsection
 
 @section('content')
+  <!-- サイドバー(コンポーネント) -->
+  @component('components.menu_book')
+  @endcomponent
   <div class="index-content">
-    <!-- サイドバー(コンポーネント) -->
-    @component('components.sidebar')
-    @endcomponent
-
     <div class="books-list">
       <div class="books-list__title">
         検索
