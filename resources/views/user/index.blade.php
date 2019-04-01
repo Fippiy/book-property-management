11d9aaf7859a9786ede5eb6292f620983d047b68
Fippiy <profier4@gmail.com>
2019-04-01 21:16:31 +0900

    create user index view

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.layout')
+
+@section('title', 'MyPage')
+
+@section('stylesheet')
+  <link href="/css/mypage.css" rel="stylesheet" type="text/css">
+@endsection
+
+@section('content')
+  <!-- サイドバー(コンポーネント) -->
+  @component('components.menu_mypage')
+  @endcomponent
+
+  <div class="mypage-content">
+    <div class="mypage-content__left">
+      <div class="mypage-content__left--box">
+        <h2 class="mypage-label__top">ユーザー情報</h2>
+      </div>
+      <div class="mypage-content__left--box">
+        <h2 class="mypage-label">ユーザー名</h2>
+        <span class="mypage-element">{{$user->name}}</span>
+      </div>
+    </div>
+    <div class="mypage-content__right">
+      <div class="my-book__title">
+        所有書籍一覧
+      </div>
+
+      <!-- テンプレート化予定 -->
+      <div class="my-book__list">
+        <div class="my-book__list--picture">
+          <img src="../image/no-entry.jpg">
+        </div>
+        <div class="my-book__list--detail">
+          <h3 class="book-title">仮置き一覧タイトルその1</h3>
+        </div>
+      </div>
+      <!-- テンプレート化予定ここまで -->
+
+
+    </div>
+  </div>
+@endsection
