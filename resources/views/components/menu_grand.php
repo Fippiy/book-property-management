528b5cdc38e07855e4994f7825d966f35a99b49e
Fippiy <profier4@gmail.com>
2019-04-02 20:47:10 +0900

    create grand menu

---
+++ resources/views/components/menu_grand.php
@@ -0,0 +1,12 @@
+<div class="book-header__menutab">
+  <a href="/book">
+    <div class="tab book">
+      登録書籍
+    </div>
+  </a>
+  <a href="/user">
+    <div class="tab mypage">
+      マイページ
+    </div>
+  </a>
+</div>
