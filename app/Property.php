4600c282802664c7fa3663dd53e95fb14c5b838d
Fippiy <profier4@gmail.com>
2019-04-08 21:42:36 +0900

    setting property model

--- app/Property.php
+++ app/Property.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    //
+  protected $guarded = array('id');
+  public static $rules = array(
+    'user_id' => 'required',
+    'bookdata_id' => 'required'
+  )
 }
