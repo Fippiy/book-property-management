23e19f1eee543e12b8fa32aec33bb04ff54f3436
Fippiy <profier4@gmail.com>
2019-04-09 18:33:43 +0900

    rewrite user index view to change class and component

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'MyPage')
 
 @section('stylesheet')
-  <link href="/css/mypage.css" rel="stylesheet" type="text/css">
+  <link href="/css/book-index.css" rel="stylesheet" type="text/css">
 @endsection
 
 @section('breadcrumbs')
@@ -16,32 +16,13 @@
   <!-- サブメニュー(コンポーネント) -->
   @component('components.menu_mypage')
   @endcomponent
-
-  <div class="mypage-content">
-    <div class="mypage-content__left">
-      <div class="mypage-content__left--box">
-        <h2 class="mypage-label__top">ユーザー情報</h2>
-      </div>
-      <div class="mypage-content__left--box">
-        <h2 class="mypage-label">ユーザー名</h2>
-        <span class="mypage-element">{{$user->name}}</span>
-      </div>
-    </div>
-    <div class="mypage-content__right">
-      <div class="my-book__title">
+  <div class="index-content">
+    <div class="books-list">
+      <div class="books-list__title">
         所有書籍一覧
       </div>
-
-      <div class="my-book__list">
-        <div class="my-book__list--picture">
-          <img src="../image/no-entry.jpg">
-        </div>
-        <div class="my-book__list--detail">
-          <h3 class="book-title">{{$property->bookdata->title}}</h3>
-        </div>
-      </div>
-
-
+      @include('components.books_list')
     </div>
+
   </div>
 @endsection
