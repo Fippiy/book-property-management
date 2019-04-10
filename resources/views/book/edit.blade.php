628e7a1f02d776f810fb9ca4ad76ff3d3c64c1c5
Fippiy <profier4@gmail.com>
2019-04-10 13:12:54 +0900

    label setting lavelstyle to whole stylesheet

--- resources/views/book/edit.blade.php
+++ resources/views/book/edit.blade.php
@@ -5,9 +5,7 @@
 @section('stylesheet')
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script src="/js/update_book.js" type="text/javascript" charset="UTF-8"></script>
-  <link href="/css/sidebar.css" rel="stylesheet" type="text/css">
   <link href="/css/book-index.css" rel="stylesheet" type="text/css">
-  <link href="/css/label.css" rel="stylesheet" type="text/css">
 @endsection
 
 @section('breadcrumbs')
