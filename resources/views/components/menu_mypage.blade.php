8f5918a0df5f1ce74af902a15ca21bef06ca42c6
Fippiy <profier4@gmail.com>
2019-04-10 13:03:21 +0900

    rewrite color class

--- resources/views/components/menu_mypage.blade.php
+++ resources/views/components/menu_mypage.blade.php
@@ -1,16 +1,16 @@
 <div class="book-header__menutab">
   <a href="/user">
-    <div class="tab mypage">
+    <div class="tab mypage-color">
       マイページトップ
     </div>
   </a>
-  <div class="tab mypage">
+  <div class="tab mypage-color">
     ユーザ情報編集
   </div>
-  <div class="tab mypage">
+  <div class="tab mypage-color">
     所有書籍登録
   </div>
-  <div class="tab mypage">
+  <div class="tab mypage-color">
     所有書籍検索
   </div>
 </div>
