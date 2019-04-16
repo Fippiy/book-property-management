97f40b94f5399b17d55c3a87b908c34fb81c693c
Fippiy <profier4@gmail.com>
2019-04-16 11:59:41 +0900

    rewrite route settings

--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,36 @@
 |
 */
 
+// ログインルート
+Auth::routes();
+
+// ログインルートに巻き取り後削除
+Route::post('/user/signup', 'UserController@postSignup');
+Route::get('/user/signup', 'UserController@getSignup');
+Route::post('/user/login', 'UserController@postLogin');
+Route::get('/user/login', 'UserController@getLogin');
+
+// 削除[ビューとコントローラ削除後]
 Route::get('/', function () {
     // return view('welcome');
   return view('index');
 });
 
-Route::get('/book/isbn', 'BookController@getIsbn')->middleware('auth');
-Route::post('/book/isbn', 'BookController@postIsbn')->middleware('auth');
-Route::get('/book/find', 'BookController@find')->middleware('auth');
-Route::post('/book/find', 'BookController@search')->middleware('auth');
-Route::resource('book', 'BookController')->middleware('auth');
-Route::get('/property/find', 'PropertyController@find')->middleware('auth');
-Route::post('/property/find', 'PropertyController@search')->middleware('auth');
-Route::resource('property', 'PropertyController')->middleware('auth');
-Route::post('/user/signup', 'UserController@postSignup');
-Route::get('/user/signup', 'UserController@getSignup');
-Route::post('/user/login', 'UserController@postLogin');
-Route::get('/user/login', 'UserController@getLogin');
-Route::resource('user', 'UserController')->middleware('auth');
+// ログイン必須ページ
+Route::group(['middleware' => ['auth']], function () {
+  Route::get('/book/isbn', 'BookController@getIsbn');
+  Route::post('/book/isbn', 'BookController@postIsbn');
+  Route::get('/book/find', 'BookController@find');
+  Route::post('/book/find', 'BookController@search');
+  Route::resource('book', 'BookController');
+  Route::get('/property/find', 'PropertyController@find');
+  Route::post('/property/find', 'PropertyController@search');
+  Route::resource('property', 'PropertyController');
+  Route::resource('user', 'UserController');
+});
 
-Auth::routes();
-
+// 削除[ビューとコントローラ削除後]
 Route::get('/home', 'HomeController@index')->name('home');
 
-// Route::get('/info', function () {
-//     phpinfo();
-// });
+// info情報表示用
+// Route::get('/info', function () { phpinfo(); });
