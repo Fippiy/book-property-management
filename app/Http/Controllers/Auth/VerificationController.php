71f4d0a7d9f9d081f7355c4cbe3af4ae56d27884
Fippiy <profier4@gmail.com>
2019-04-19 14:47:21 +0900

    edit verified redirect pass

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/book';
 
     /**
      * Create a new controller instance.
