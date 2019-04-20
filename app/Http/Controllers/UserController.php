4186e6a5e63c8ea813202c57953b7d2e4483ebf4
Fippiy <profier4@gmail.com>
2019-04-20 11:50:53 +0900

    setting user edit controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -60,7 +60,8 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+      $auth = Auth::user();
+      return view('user.edit',[ 'auth' => $auth ]);
     }
 
     /**
@@ -72,7 +73,29 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-      //
+      // バリデーションルール
+      $this->validate($request, User::$editRules);
+      // 対象レコード取得
+      $auth = User::find($id);
+      // リクエストデータ受取
+      $form = $request->all();
+      // フォームトークン削除
+      unset($form['_token']);
+      // フォーム要素の評価
+      foreach ($form as $key => $value) {
+        // nullの場合更新対象から除外する
+        if($value == null) {
+          unset($form[$key]);
+        }
+      }
+      // パスワード処理
+      // ハッシュ化
+      if (isset($form['password'])) {
+        $form['password'] = Hash::make($form['password']);
+      }
+      // レコードアップデート
+      $auth->fill($form)->save();
+      return redirect('/user');
     }
 
     /**
