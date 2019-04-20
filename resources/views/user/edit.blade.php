93de16c0bf64863e66f583ee9730f519eb1daa2c
Fippiy <profier4@gmail.com>
2019-04-20 17:11:33 +0900

    rewrite user edit page view

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -21,7 +21,6 @@
 @foreach ($errors->all() as $error)
   <p class="auth-contents__message--error">{{ $error }}</p>
 @endforeach
-
 <div class="index-content">
     <div class="books-list">
       <div class="books-list__title mypage-color">
@@ -33,33 +32,40 @@
         </div>
       @endif
       <div class="book-new">
-          <form action="{{ route('user.update', $auth->id)}}" method="post" enctype="multipart/form-data">
-            {{ csrf_field() }}
-            <input type="hidden" name="_method" value="PUT">
-            <div class="form-contents">
-              <div class="form-one-size">
-                <div class="form-input">
-                  <div class="form-label">ユーザー名</div>
-                  <div><input class="form-input__input" type="text" name="name" value="{{$auth->name}}"></div>
-                </div>
-                <div class="form-input">
-                  <div class="form-label">メールアドレス</div>
-                  <div><input class="form-input__input" type="text" name="email" value="{{$auth->email}}"></div>
-                </div>
-                <div class="form-input">
-                  <div class="form-label">パスワード</div>
-                  <div><input class="form-input__input" type="password" name="password" value=""></div>
-                </div>
-                <div class="form-input">
-                  <div class="form-label">パスワード(確認)</div>
-                  <div><input class="form-input__input" type="password" name="password_confirmation" value=""></div>
-                </div>
+        <form action="{{ route('update.user', $auth->id)}}" method="post" enctype="multipart/form-data">
+          {{ csrf_field() }}
+          <input type="hidden" name="_method" value="PUT">
+          <input type="hidden" name="page" value="{{$page}}">
+          <div class="form-contents">
+            <div class="form-one-size">
+              @if ($page == 'name')
+              <div class="form-input">
+                <div class="form-label">ユーザー名</div>
+                <div><input class="form-input__input" type="text" name="name" value="{{$auth->name}}"></div>
               </div>
+              @endif
+              @if ($page == 'email')
+              <div class="form-input">
+                <div class="form-label">メールアドレス</div>
+                <div><input class="form-input__input" type="text" name="email" value="{{$auth->email}}"></div>
+              </div>
+              @endif
+              @if ($page == 'password')
+              <div class="form-input">
+                <div class="form-label">新パスワード</div>
+                <div><input class="form-input__input" type="password" name="password" value=""></div>
+              </div>
+              <div class="form-input">
+                <div class="form-label">新パスワード(確認)</div>
+                <div><input class="form-input__input" type="password" name="password_confirmation" value=""></div>
+              </div>
+              @endif
             </div>
-            <div class="form-foot">
-              <input class="send" type="submit" value="編集">
-            </div>
-          </form>
+          </div>
+          <div class="form-foot">
+            <input class="send" type="submit" value="編集">
+          </div>
+        </form>
       </div>
     </div>
   </div>
