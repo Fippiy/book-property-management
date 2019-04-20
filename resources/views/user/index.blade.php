7c8ce7be0afe099e0aa1ae0ef0e04c4fd352bf92
Fippiy <profier4@gmail.com>
2019-04-20 17:08:45 +0900

    setting edit button to user index page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -26,16 +26,23 @@
       <div class="book-table">
         <div class="book-table__profile-list">
           <div class="profile-group">
-            <div class="profile-group__title">ユーザー名</div>
-            <div class="profile-group__element">{{$auth->name}}</div>
-          </div>
-          <div class="profile-group">
             <div class="profile-group__title">ユーザーID</div>
             <div class="profile-group__element">{{$auth->id}}</div>
           </div>
           <div class="profile-group">
+            <div class="profile-group__title">ユーザー名</div>
+            <div class="profile-group__element">{{$auth->name}}</div>
+            <div class="profile-group__edit"><a href="{{ route('edit.user', 'name') }}">編集</a></div>
+          </div>
+          <div class="profile-group">
             <div class="profile-group__title">メールアドレス</div>
             <div class="profile-group__element">{{$auth->email}}</div>
+            <div class="profile-group__edit"><a href="{{ route('edit.user', 'email') }}">編集</a></div>
+          </div>
+          <div class="profile-group">
+            <div class="profile-group__title">パスワード</div>
+            <div class="profile-group__element">パスワードは安全の為表示できません。</div>
+            <div class="profile-group__edit"><a href="{{ route('edit.user', 'password') }}">編集</a></div>
           </div>
           <div class="profile-group">
             <div class="profile-group__title">登録日時</div>
