e0c21a50f16d41ae9d1489a6a2d33ed60c775d3e
Fippiy <profier4@gmail.com>
2019-04-20 17:11:48 +0900

    setting update validate to user model

--- app/User.php
+++ app/User.php
@@ -52,7 +52,13 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         $this->notify(new CustomVerifyEmail());
     }
-    public static $editRules = array(
-        'password' => 'confirmed'
+    public static $editNameRules = array(
+        'name' => 'required|max:255'
+    );
+    public static $editEmailRules = array(
+        'email' => 'required|email'
+    );
+    public static $editPasswordRules = array(
+        'password' => 'confirmed|min:8'
     );
 }
