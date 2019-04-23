4da4865df7d179856b6889381c018fd613019354
Fippiy <profier4@gmail.com>
2019-04-23 22:53:44 +0900

    create startup user test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,18 +5,47 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
 
 class UserTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testAccess()
     {
-        $response = $this->get('/');
+        $this->assertTrue(true);
 
-        $response->assertStatus(200);
+        $response = $this->get('/'); // ルートにアクセス
+        $response->assertStatus(200); // 200ステータスであること
+
+        $response = $this->get('/user'); // ~/userにアクセス
+        $response->assertStatus(302);  // 302ステータスであること
+
+        $user = factory(User::class)->create(); // User作成
+        $response = $this->actingAs($user)->get('/user'); // 作成ユーザーでログインして~/userにアクセス
+        $response->assertStatus(200); // 200ステータスであること
+
+        $response = $this->get('/no_route'); // ページのないアドレスへアクセス
+        $response->assertStatus(404); // 404ステータスであること
+    }
+    public function testDatabase()
+    {
+        factory(User::class)->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]); // DBに配列で指定したユーザーを生成
+        factory(User::class, 10)->create(); // UserFactory指定のユーザーを10レコード作成
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]); //DBに配列で指定したユーザーがいること
     }
 }
