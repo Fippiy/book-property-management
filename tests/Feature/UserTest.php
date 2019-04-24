d493a821eed8eb460bf4434e759b419e628a0677
Fippiy <profier4@gmail.com>
2019-04-24 16:40:18 +0900

    setting reset password check

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Notification;
+use App\Notifications\CustomResetPassword;
+use Illuminate\Support\Facades\Hash;
 
 class UserTest extends TestCase
 {
@@ -57,7 +60,7 @@ class UserTest extends TestCase
         ]); //DBに配列で指定したユーザーがいること
     }
 
-    // ログイン成功テスト
+    // ログイン成功
     public function testLoginOK()
     {
     $user = factory(User::class)->create(); // ユーザーを作成
@@ -74,8 +77,8 @@ class UserTest extends TestCase
     $response->assertRedirect('book'); // ログイン後にリダイレクトされるのを確認
     }
 
-    // ログイン失敗テスト
-    public function testLoginNgNotPass()
+    // ログイン失敗パスワードNG
+    public function testLoginNotPassNg()
     {
         $user = factory(User::class)->create(); // ユーザーを作成
 
@@ -91,7 +94,9 @@ class UserTest extends TestCase
         $this->assertEquals('認証情報が記録と一致しません。',
         session('errors')->first('email')); // エラメッセージを確認
     }
-    public function testLoginNgNotName()
+
+    // ログイン失敗emailNG
+    public function testLoginNotNameNg()
     {
         $user = factory(User::class)->create(); // ユーザーを作成
 
@@ -107,5 +112,54 @@ class UserTest extends TestCase
 
         $this->assertEquals('認証情報が記録と一致しません。',
         session('errors')->first('email')); // エラメッセージを確認
+    }
+
+    // リセットパスワード成功
+    public function testResetPasswordOk()
+    {
+      Notification::fake(); // 通知機能使用、実際には通知しない
+
+      $response = $this->get('password/reset'); // パスワードリセットページにアクセス    
+      $response->assertStatus(200); // 正常表示されること
+
+      $user = factory(User::class)->create(); // テストユーザー準備
+ 
+      $response = $this->from('password/email')->post('password/email', [
+          'email' => $user->email,
+      ]); // リセットをリクエスト
+
+      $response->assertStatus(302); // リダイレクトされること
+      $response->assertRedirect('password/email'); // 同画面にリダイレクト
+
+      $response->assertSessionHas('status',
+         'パスワード再設定用のURLをメールで送りました。'); // 成功のメッセージ
+
+      $token = '';
+      Notification::assertSentTo(
+          $user,
+          CustomResetPassword::class,
+          function ($notification, $channels) use ($user, &$token) {
+              $token = $notification->token;
+              return true;
+          }
+      ); // メール通知を実施(fackにより実際にメールはしない)
+
+      $response = $this->get('password/reset/'.$token); // リセットURLページへアクセス
+      $response->assertStatus(200); // アクセスできること
+ 
+      $new = 'reset1111'; // 新しいパスワード
+      $response = $this->post('password/reset', [
+         'email'                 => $user->email,
+         'token'                 => $token,
+         'password'              => $new,
+         'password_confirmation' => $new
+       ]); // 新パスワードで登録
+
+     $response->assertStatus(302); // リダイレクト
+     $response->assertRedirect('/login');  // ログインページに遷移
+
+     $this->assertTrue(Auth::check()); // 認証されていることを確認
+
+     $this->assertTrue(Hash::check($new, $user->fresh()->password)); // 変更されたパスワードが保存されていることを確認
     }
 }
