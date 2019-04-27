74e4399c47f43781d77902b0ae3157856541f584
Fippiy <profier4@gmail.com>
2019-04-27 10:39:51 +0900

    create bookdata test to index access

--- tests/Feature/BookdataTest.php
+++ tests/Feature/BookdataTest.php
@@ -5,18 +5,26 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class BookdataTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    // bookdata新規登録
+    public function test_indexAccess_ok()
     {
-        $response = $this->get('/');
+        $user = factory(User::class)->create(); // ユーザーを作成
+        $this->actingAs($user); // ログイン済み
+        $this->assertTrue(Auth::check()); // Auth認証済であることを確認
 
-        $response->assertStatus(200);
+        $response = $this->get('/book'); // bookへアクセス
+        $response->assertStatus(200); // 200ステータスであること
     }
 }
