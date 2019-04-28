46dd207b79995295a516b69b56b29798170cf01f
Fippiy <profier4@gmail.com>
2019-04-28 14:34:20 +0900

    rewrite bookdata factory

--- database/factories/BookdataFactory.php
+++ database/factories/BookdataFactory.php
@@ -5,8 +5,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Bookdata::class, function (Faker $faker) {
     return [
-        'title' => $faker->name,
         'detail' => $faker->name,
-        'isbn' => $faker->unique()->isbn13
+        'isbn' => $faker->unique()->isbn13,
+        'title' => $faker->name,
+        'volume' => $faker->name,
+        'series' => $faker->name,
+        'publisher' => $faker->name,
+        'pubdate' => $faker->date,
+        'author' => $faker->name
     ];
 });
