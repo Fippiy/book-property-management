9288d4a11a46cdeb3d63467a5c38dd71bf7d9b69
Fippiy <profier4@gmail.com>
2019-04-28 14:15:47 +0900

    create bookdata factory base

--- database/factories/BookdataFactory.php
+++ database/factories/BookdataFactory.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Bookdata;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Bookdata::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->name,
+        'detail' => $faker->name,
+        'isbn' => $faker->unique()->isbn13
     ];
 });
