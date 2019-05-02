71412c9492422f6f08ff9a63348fe08ced9bc235
Fippiy <profier4@gmail.com>
2019-05-02 18:01:12 +0900

    comment out validate rule to property controller store

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -43,8 +43,8 @@ class PropertyController extends Controller
     public function store(Request $request)
     {
       // バリデーションチェック
-      $createPropertyRules = Property::createPropertyRules();
-      $this->validate($request, $createPropertyRules);
+      // $createPropertyRules = Property::createPropertyRules();
+      // $this->validate($request, $createPropertyRules);
       // 新規レコード生成
       $property = new Property;
       // リクエストデータ受取
