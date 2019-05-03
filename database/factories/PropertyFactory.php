3d04b1abcf40481e4ebafd5e3655e6f2cb8fdf9b
Fippiy <profier4@gmail.com>
2019-05-03 14:43:25 +0900

    create property factory

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Property;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Property::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => 1,
+        'bookdata_id' => 1,
+        'number' => 1,
+        'getdate' => $faker->date,
+        'freememo' => $faker->name,
     ];
 });
