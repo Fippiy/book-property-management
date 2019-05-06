a2817161973ce4719649d7dba3147eadb0197b9e
Fippiy <profier4@gmail.com>
2019-05-06 13:32:42 +0900

    access ok test

--- tests/Feature/PropertyTest.php
+++ tests/Feature/PropertyTest.php
@@ -5,18 +5,25 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class PropertyTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    public function test_indexAccess_ok()
     {
-        $response = $this->get('/');
+        $user = factory(User::class)->create(); // ユーザーを作成
+        $this->actingAs($user); // ログイン済み
+        $this->assertTrue(Auth::check()); // Auth認証済であることを確認
 
-        $response->assertStatus(200);
+        $response = $this->get('/property'); // propertyへアクセス
+        $response->assertStatus(200); // 200ステータスであること
     }
 }
