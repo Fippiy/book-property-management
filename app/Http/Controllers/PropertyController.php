0f6e6f1ec90807a1a6eb591989fc2b9e8e0c7dd7
Fippiy <profier4@gmail.com>
2019-05-13 17:09:49 +0900

    count entry property data setting

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -57,10 +57,10 @@ class PropertyController extends Controller
                         ->where('bookdata_id', $form['bookdata_id'])
                         ->first();
       // 書籍があればFalse
-      if (count($entry_property) == 0) {
-        $have_property = true;
-      } else {
+      if (count($entry_property) > 0) {
         $have_property = false;
+      } else {
+        $have_property = true;
       }
       // False時にエラーとして返す
       $validator = Validator::make(['bookdata_id' => $have_property], ['bookdata_id' => 'accepted'], ['書籍は登録済みです']);
