9e436eba03cd79da56b6b331ba0d47fc3d0c486c
Fippiy <profier4@gmail.com>
2019-05-13 17:22:37 +0900

    rewrite check count

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -57,12 +57,11 @@ class PropertyController extends Controller
                         ->where('bookdata_id', $form['bookdata_id'])
                         ->first();
       // 書籍があればFalse
-      if (count($entry_property) == null) {
+      if ($entry_property == null) {
         $have_property = true;
       } else {
         $have_property = false;
       }
-      eval(\Psy\sh());
       // False時にエラーとして返す
       $validator = Validator::make(['bookdata_id' => $have_property], ['bookdata_id' => 'accepted'], ['書籍は登録済みです']);
       if ($validator->fails()) {
