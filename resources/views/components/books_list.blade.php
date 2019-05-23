e72f3b0dbdebb4e781eff7ac87b827f5a1e5053d
Fippiy <profier4@gmail.com>
2019-05-23 17:53:46 +0900

    delete form setting to books_list page

--- resources/views/components/books_list.blade.php
+++ resources/views/components/books_list.blade.php
@@ -1,23 +1,37 @@
 <div class="book-table">
   @if (isset($books))
-    @foreach ($books as $book)
-      <div class="book-table__list">
-        <div class="book-table__list--picture">
-          <a href="/{{$page_path}}/{{$book->id}}">
-            @if (isset($book->picture))
-              <img src="{{$book->picture}}">
-            @elseif (isset($book->cover))
-              <img src="{{$book->cover}}">
-            @else
-              <img src="../image/no-entry.jpg">
-            @endif
-          </a>
-        </div>
-        <div class="book-table__list--detail">
-          <a href="/{{$page_path}}/{{$book->id}}"><h3 class="list-book-title">{{$book->title}}</h3></a>
-          <p class="list-book-detail">{{ str_limit($book->$detail, $limit = 300, $end = '...') }}</p>
-        </div>
+    @if ($page_path == 'book')
+      <form action="{{ route('book.some_delete') }}" method="post">
+    @else
+      <form action="{{ route('property.some_delete') }}" method="post">
+    @endif
+      <div class="book-table__btn">
+        <span>操作：</span>
+        <input type="submit" class="book-table__btn--delete" value="書籍情報一括削除">
       </div>
-    @endforeach
+      {{ csrf_field() }}
+      @foreach ($books as $book)
+        <div class="book-table__list">
+          <div class="book-table__list--checkbox">
+            <input type="checkbox" name="{{$book->id}}">
+          </div>
+          <div class="book-table__list--picture">
+            <a href="/{{$page_path}}/{{$book->id}}">
+              @if (isset($book->picture))
+                <img src="{{$book->picture}}">
+              @elseif (isset($book->cover))
+                <img src="{{$book->cover}}">
+              @else
+                <img src="../image/no-entry.jpg">
+              @endif
+            </a>
+          </div>
+          <div class="book-table__list--detail">
+            <a href="/{{$page_path}}/{{$book->id}}"><h3 class="list-book-title">{{$book->title}}</h3></a>
+            <p class="list-book-detail">{{ str_limit($book->$detail, $limit = 300, $end = '...') }}</p>
+          </div>
+        </div>
+      @endforeach
+    </form>
   @endif
 </div>
